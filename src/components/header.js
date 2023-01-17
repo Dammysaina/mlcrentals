8db2e486b0d2ba13ab627859109fc04c90f1e157
Oluwadamilola Adediran <dammyadediran94@gmail.com>
2023-01-18 00:41:03 +0100

    packages installed

---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+import React from "react"
+ 
+const Header = () => {
+    return (
+        <div>
+            <div></div>
+            <div>
+                <ul>
+                    <li></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Header
