83ce34d562da5a09845a7397b7b4d1e18e37abce
Oluwadamilola Adediran <dammyadediran94@gmail.com>
2023-01-18 02:07:26 +0100

    home

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,17 @@
-import React from "react"
+import React from "react";
+import Logo from "../../public/assests/images/logo.svg";
+import {Link} from "react-router-dom"
  
 const Header = () => {
     return (
         <div>
-            <div></div>
+            <div><img src={Logo} alt="mlcLogo"/></div>
             <div>
                 <ul>
-                    <li></li>
+                    <Link to="/"><li>Home</li></Link>
+                    <Link to="/"><li>Landlord</li></Link>
+                    <Link to="/"><li>Tenants</li></Link>
+                    <Link to="/"><li>Contact Us</li></Link>
                 </ul>
             </div>
         </div>
---
+++ src/components/home.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+
+
+const Home = () => {
+    return (
+        <div className="home_container">
+            <div>
+                <p>The most affortable place to stay in the san franciso bay area</p>
+            </div>
+            <div>
+                <div>
+
+                </div>
+                <div>
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
