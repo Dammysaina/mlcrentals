2dc451f129372ffe8cf4a9259c1c07b1db347a8f
Oluwadamilola Adediran <dammyadediran94@gmail.com>
2023-01-19 09:40:14 +0100

    home

--- src/components/home.js
+++ src/components/home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Map from "./map";
 
 
 
@@ -10,7 +11,7 @@ const Home = () => {
             </div>
             <div>
                 <div>
-
+                    <Map />
                 </div>
                 <div>
                     
