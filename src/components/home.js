d06383d3ed9cd3d2d2eac1561c9dd6b000ffbc22
Oluwadamilola Adediran <dammyadediran94@gmail.com>
2023-02-28 18:26:31 +0100

    home background

--- src/components/home.js
+++ src/components/home.js
@@ -1,33 +1,49 @@
 import React from "react";
 import Map from "./map";
-import "../assests/css/home.css"
-
-
+import "../assests/css/home.css";
 
 const Home = () => {
-    return (
-        <div className="home_container">
-            <div className="affordable_place">
-                <p>The Most Affortable Place To Stay In The San Franciso Bay Area</p>
-            </div>
-            <div className="map_select">
-                <div className="map_container">
-                    <Map alt="map" />
-                </div>
-                <div className="select_container">
-                   <div>
-
-                    </div>
-                    <div>
-
-                    </div>
-                    <div>
-                        
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="home_container">
+      <div className="affordable_place">
+        <p>The Most Affortable Place To Stay In The San Franciso Bay Area</p>
+      </div>
+      <div className="map_select">
+        <div className="map_container">
+          <Map alt="map" />
         </div>
-    )
-}
+        <div className="select_container">
+          <div>
+            <select name="languages" id="lang" placeholder="All types">
+              <option value="javascript">All Type</option>
+              <option value="php">PHP</option>
+              <option value="java">Java</option>
+              <option value="golang">Golang</option>
+              <option value="python">Python</option>
+              <option value="c#">C#</option>
+              <option value="C++">C++</option>
+              <option value="erlang">Erlang</option>
+            </select>
+          </div>
+          <div>
+            <select name="languages" id="lang" placeholder="All types">
+              <option value="javascript">Neighborhood</option>
+              <option value="php">PHP</option>
+              <option value="java">Java</option>
+              <option value="golang">Golang</option>
+              <option value="python">Python</option>
+              <option value="c#">C#</option>
+              <option value="C++">C++</option>
+              <option value="erlang">Erlang</option>
+            </select>
+          </div>
+          <div className="select_image">
+            <img src="" alt="searchbutton" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
