9d9b1fdc4354eb7eea2b4cb7db658df45c6f38f4
Oluwadamilola Adediran <dammyadediran94@gmail.com>
2023-03-03 15:37:04 +0100

    minimum

--- src/components/home.js
+++ src/components/home.js
@@ -1,58 +1,72 @@
 import React from "react";
 import Map from "./map";
 import "../assests/css/home.css";
-import SEARCH from "../assests/images/search.svg"
+import SEARCH from "../assests/images/search.svg";
 
 const Home = () => {
   return (
     <div>
-        <div className="home_container">
-      <div className="affordable_place">
-        <p>The Most Affortable Place To Stay In The San Franciso Bay Area</p>
-      </div>
-      <div className="map_select">
-        <div className="map_container">
-          <Map alt="map" />
+      <div className="home_container">
+        <div className="affordable_place">
+          <p>The Most Affortable Place To Stay In The San Franciso Bay Area</p>
         </div>
-        <div className="select_container">
-          <div>
-            <select name="languages" id="lang" className="select_neighborhoodone">
-              <option value="javascript">All Type</option>
-              <option value="php">PHP</option>
-              <option value="java">Java</option>
-              <option value="golang">Golang</option>
-              <option value="python">Python</option>
-              <option value="c#">C#</option>
-              <option value="C++">C++</option>
-              <option value="erlang">Erlang</option>
-            </select>
+        <div className="map_select">
+          <div className="map_container">
+            <Map alt="map" />
           </div>
-          <div>
-            <select name="languages" id="lang" className="select_neighborhoodtwo">
-              <option value="javascript">Neighborhood</option>
-              <option value="php">PHP</option>
-              <option value="java">Java</option>
-              <option value="golang">Golang</option>
-              <option value="python">Python</option>
-              <option value="c#">C#</option>
-              <option value="C++">C++</option>
-              <option value="erlang">Erlang</option>
-            </select>
-          </div>
-          <div className="select_image">
-            <img src={SEARCH} alt="searchbutton" />
+          <div className="select_container">
+            <div>
+              <select
+                name="languages"
+                id="lang"
+                className="select_neighborhoodone"
+              >
+                <option value="javascript">All Type</option>
+                <option value="php">PHP</option>
+                <option value="java">Java</option>
+                <option value="golang">Golang</option>
+                <option value="python">Python</option>
+                <option value="c#">C#</option>
+                <option value="C++">C++</option>
+                <option value="erlang">Erlang</option>
+              </select>
+            </div>
+            <div>
+              <select
+                name="languages"
+                id="lang"
+                className="select_neighborhoodtwo"
+              >
+                <option value="javascript">Neighborhood</option>
+                <option value="php">PHP</option>
+                <option value="java">Java</option>
+                <option value="golang">Golang</option>
+                <option value="python">Python</option>
+                <option value="c#">C#</option>
+                <option value="C++">C++</option>
+                <option value="erlang">Erlang</option>
+              </select>
+            </div>
+            <div className="select_image">
+              <img src={SEARCH} alt="searchbutton" />
+            </div>
           </div>
         </div>
       </div>
-      
-    </div>
-    <div>
+      <div className="minimum_container">
+        <div className="minimum_living">
+          <p>Minimum Living Cost Takes Care Of Everything</p>
+        </div>
         <div>
-            <p>minimum living cost takes care of everything</p>
+          <div>
+            <img src="" alt=" house"/>
+          </div>
+          <div>
+
+          </div>
         </div>
       </div>
     </div>
-    
   );
 };
 
