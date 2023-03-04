33ef0917e6c7b17bdd6953b159d2a2766ccf621c
Oluwadamilola Adediran <dammyadediran94@gmail.com>
2023-03-04 21:37:09 +0100

    selection

--- src/components/selection.js
+++ src/components/selection.js
@@ -10,37 +10,37 @@ const Selection = [
         id:1,
         image: Dollars,
         header: "BERTHING AND MOORING",
-        body: "Consequat Lorem tempor Lorem officia reprehenderit laboris incididunt eiusmod id Lorem nostrud."
+        body: "Pay As Little As Possible!"
     },
     {
         id:2,
         image: House,
         header: "MARINE SERVICES",
-        body: "Magna quis laborum nulla incididunt cillum laboris deserunt aliqua non proident. Sunt et ut et aliqua consequat nulla fugiat mollit est irure nisi deserunt esse esse. Dolore magna quis sunt ea non Lorem. Incididunt velit voluptate do proident et proident nostrud sit voluptate. Eu qui non qui quis adipisicing non non eiusmod fugiat exercitation officia do consequat reprehenderit. Eu id aliqua magna enim duis ad nisi aliquip. Esse anim minim est tempor ea eu sunt do nostrud aliqua proident ex minim."
+        body: "Enjoy Wisdom Of Community!"
     },
     {
         id:3,
         image: Help,
         header: "MARINE SERVICES",
-        body: "Magna quis laborum nulla incididunt cillum laboris deserunt aliqua non proident. Sunt et ut et aliqua consequat nulla fugiat mollit est irure nisi deserunt esse esse. Dolore magna quis sunt ea non Lorem. Incididunt velit voluptate do proident et proident nostrud sit voluptate. Eu qui non qui quis adipisicing non non eiusmod fugiat exercitation officia do consequat reprehenderit. Eu id aliqua magna enim duis ad nisi aliquip. Esse anim minim est tempor ea eu sunt do nostrud aliqua proident ex minim."
+        body: " Let's Somebody Else Take Care Of Landlord!"
     },
     {
         id:4,
         image: Plant,
         header: "MARINE SERVICES",
-        body: "Magna quis laborum nulla incididunt cillum laboris deserunt aliqua non proident. Sunt et ut et aliqua consequat nulla fugiat mollit est irure nisi deserunt esse esse. Dolore magna quis sunt ea non Lorem. Incididunt velit voluptate do proident et proident nostrud sit voluptate. Eu qui non qui quis adipisicing non non eiusmod fugiat exercitation officia do consequat reprehenderit. Eu id aliqua magna enim duis ad nisi aliquip. Esse anim minim est tempor ea eu sunt do nostrud aliqua proident ex minim."
+        body: " Enjoy Peaceful Environment!"
     },
     {
         id:5,
         image: Safety,
         header: "MARINE SERVICES",
-        body: "Magna quis laborum nulla incididunt cillum laboris deserunt aliqua non proident. Sunt et ut et aliqua consequat nulla fugiat mollit est irure nisi deserunt esse esse. Dolore magna quis sunt ea non Lorem. Incididunt velit voluptate do proident et proident nostrud sit voluptate. Eu qui non qui quis adipisicing non non eiusmod fugiat exercitation officia do consequat reprehenderit. Eu id aliqua magna enim duis ad nisi aliquip. Esse anim minim est tempor ea eu sunt do nostrud aliqua proident ex minim."
+        body: " Stay Safe! Save Money! "
     },
     {
         id:6,
         image: Moon,
         header: "MARINE SERVICES",
-        body: "Magna quis laborum nulla incididunt cillum laboris deserunt aliqua non proident. Sunt et ut et aliqua consequat nulla fugiat mollit est irure nisi deserunt esse esse. Dolore magna quis sunt ea non Lorem. Incididunt velit voluptate do proident et proident nostrud sit voluptate. Eu qui non qui quis adipisicing non non eiusmod fugiat exercitation officia do consequat reprehenderit. Eu id aliqua magna enim duis ad nisi aliquip. Esse anim minim est tempor ea eu sunt do nostrud aliqua proident ex minim."
+        body: "Pay For What You Use! "
     },
 ]
 
